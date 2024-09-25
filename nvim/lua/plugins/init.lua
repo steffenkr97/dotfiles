2ad82fb2abb2ff29f0bf0811729542f64ae38302
Steffen Kroll <steffen.kroll@fiege.com>
2024-09-25 13:20:16 +0200

    added vim config

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -13,13 +13,41 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "json", "kotlin",
+        "xml",
+  		},
+  	},
+  },
+
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = false,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "work",
+          path = "~/vaults/stekroll-notes/",
+          overrides = {
+            templates = {
+              folder = "daily notes/templates",
+            },
+            daily_notes = {
+              folder = "daily notes/2024/09",
+              template = "daily-note-template.md"  },
+            },
+          }
+        }
+      },
+    }
+
 }
