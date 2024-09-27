87d571cbb9e7e03ff5ed05c779821fcec2c05c88
Steffen Kroll <steffen.kroll@fiege.com>
2024-09-27 16:23:45 +0200

    adjusted nvim tree width

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -5,3 +5,12 @@ vim.opt.conceallevel = 1
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+
+-- my vim-tree
+local nvimtree = require("nvim-tree")
+require("nvim-tree").setup({
+    view = {
+      adaptive_size = true
+    },
+  })
