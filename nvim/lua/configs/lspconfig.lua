e4b8c89efdca3f8e7af6e569176fdcc8ff47de84
Steffen Kroll <steffen.kroll@fiege.com>
2024-09-27 09:19:46 +0200

    added kotlin, dap, dapui. Also added ram to tmux

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "terraformls", "kotlin_language_server" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -79,6 +79,36 @@ return {
     keys = {
       { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"}
+    },
+    config = function ()
+      local dap, dapui = require("dap"), require("dapui")
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function(      )
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+
+      vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint(), {desc="Toggle Breaktpoint"})
+      vim.keymap.set("n", "<leader>dc", dap.continue(), {desc="Continue or debug programm"})
+    end
+
+    
   }
+
+
 
 }
