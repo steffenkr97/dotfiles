71bae459f7f009ba7c8ea77be1af62b24983c9be
Steffen Kroll <kroll.steffen@icloud.com>
2025-12-01 20:11:37 +0100

    add debugger

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,6 +29,8 @@ require('lazy').setup {
   require 'plugins.indent-blankline',
   require 'plugins.misc',
   require 'plugins.comment',
+  require 'plugins.rustacean',
+  require 'plugins.debug',
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
